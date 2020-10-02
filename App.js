9e52e6568764b170979e9c0bf5470f1a586a876b
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-10-02 16:35:15 +0530

    Added files for tutorial

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image, ToastAndroid } from 'react-native';
+import { Audio } from 'expo-av';
 
-export default function App() {
+import buzz from './assets/buzz.mp3';
+import play from './assets/media_play.png';
+
+export default class App extends React.Component {
+  constructor()
+  {
+    super();
+    this.state = { playbackObject: null }
+  }
+
+  async componentDidMount() {
+    this.setState({ playbackObject: await new Audio.Sound() });
+    try
+    {
+      this.state.playbackObject.loadAsync(buzz);
+    } catch (e) {
+      ToastAndroid.show("Error occurred! " + e,ToastAndroid.SHORT);
+    }
+  }
+
+  async playSound() {
+    try {
+    await this.state.playbackObject.stopAsync();
+    await this.state.playbackObject.playAsync();
+    } catch (e) {
+      ToastAndroid.show("Error occurred! " + e,ToastAndroid.SHORT);
+    }
+  }
+  render()
+  {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={this.playSound.bind(this)}>
+      <Image
+        style={{ width: 100, height: 100}}
+        source={play}
+      />
+      </TouchableOpacity>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
